e483fd39b2600bf09fe91ba578e884e059c0e3fc
Jonathan Lee <l33.jonathan@gmail.com>
2026-03-10 19:54:48 -0700

    React overhaul

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Modal from './components/modal';
+
+function App() {
+  return (
+    <div className="App vaporwave-bg">
+      <Modal />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/modal.tsx
@@ -0,0 +1,18 @@
+import ModalCSS from '../css/Modal.module.scss';
+
+const Modal: React.FC = () => {
+    return (
+        <div className={ModalCSS.modal}>
+            <div className={ModalCSS.modalContent}>
+                <h2>Hi there</h2>
+                <p>I am a skilled Rails and React developer looking for new opportunities and this is my page.</p>
+                <div className={ModalCSS.links}>
+                    <a href="">Resume</a>
+                    <a href="https://www.linkedin.com/in/jonathanl33">LinkedIn</a>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Modal;
